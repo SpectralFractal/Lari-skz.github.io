62867bd901ac57b49b923622ebbd5edfdbb770e2
Lari-skz <63294282+Lari-skz@users.noreply.github.com>
2023-05-03 13:41:15 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,21 +1,16 @@
 async function getRecipes(ingredients) {
-  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+  const response = await fetch('https://foodcombatchatgpt.herokuapp.com/getRecipes', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer 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'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      prompt: `Lista de ingrediente: ${ingredients.join(', ')}. Oferă o listă de posibile rețete folosind aceste ingrediente.`,
-      max_tokens: 150,
-      n: 1,
-      stop: null,
-      temperature: 0.8,
+      ingredients: ingredients
     })
   });
 
   const data = await response.json();
-  return data.choices[0].text.trim().split('\n');
+  return data.recipes;
 }
 
 document.getElementById('ingredients-form').addEventListener('submit', async (event) => {
